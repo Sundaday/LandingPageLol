220b83085199551977100c68b62f316ca558917b
Sundaday <john.koch.pro@gmail.com>
2022-10-22 10:15:18 +0200

    test vite.js

---
+++ src/pages/Home.jsx
@@ -0,0 +1,62 @@
+import {
+    Swiper,
+    SwiperSlide
+} from 'swiper/react'
+
+import SwiperCore, {
+    EffectFade,
+    Mousewheel,
+    Pagination
+} from 'swiper'
+
+SwiperCore.use([
+    Mousewheel,
+    Pagination,
+    EffectFade])
+
+import {
+    Champion,
+    ChampionDetail,
+    Welcome,
+    Trailer
+} from '../components/home-section'
+
+import { championsData } from '../assets/dummy'
+
+const swiperOptions = {
+    direction: 'vertical',
+    slidesPerView: 1,
+    spaceBetween: 0,
+    mousewheel: true,
+    pagination: true,
+    effect: 'fade',
+    speed: 1000
+}
+
+const Home = () => {
+    return (
+        <>
+            <Swiper {...swiperOptions}>
+                <SwiperSlide>
+                    {({ isActive }) => <Welcome isActive={isActive} />}
+                </SwiperSlide>
+                <SwiperSlide>
+                    {({ isActive }) => <Champion isActive={isActive} />}
+                </SwiperSlide>
+                <SwiperSlide>
+                    {({ isActive }) => <Trailer isActive={isActive} />}
+                </SwiperSlide>
+            </Swiper>
+            {
+                championsData.map((item, index) => <ChampionDetail
+                    key={index}
+                    item={item}
+                    id={index}
+                />
+                )
+            }
+        </>
+    )
+}
+
+export default Home
